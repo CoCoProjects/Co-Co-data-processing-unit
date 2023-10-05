e341e3ad579196d452c3f1655e53611f68d2a1b3
Hakan Kayra Doğan <hkayrad@gmail.com>
2023-10-05 21:53:42 +0300

    add CoCo-Barometer submodule, barometer initialization

--- src/main.cpp
+++ src/main.cpp
@@ -14,38 +14,54 @@ for such a notice.
 *************************************************************************/
 
 //* IMPORTS
-#include <Arduino.h>
-#include "initWifi.h"
-#include "SPI.h"
-#include "HTTPClient.h"
-#include <Deneyap_6EksenAtaletselOlcumBirimi.h>
 #include "CoCo-IMU.h"
+#include "CoCo-Barometer.h"
+#include "HTTPClient.h"
+#include "SPI.h"
+#include "initWifi.h"
+#include <Arduino.h>
+#include <Deneyap_6EksenAtaletselOlcumBirimi.h>
+#include "Adafruit_BMP085.h"
 
 //* CONSTANTS
 #define WIFI_SSID "NGTelemetryNetwork"
 #define WIFI_PASSWORD "99733940"
 #define HTTP_ADDRESS "http://192.168.95.150:3001/api"
-#define INTEGRATED_IMU_ADDRESS 0x6A
+#define IMU_ADDRESS 0x6A
 
 //* OBJECTS
 HTTPClient http;
-LSM6DSM i_IMU;
-IMU imu(INTEGRATED_IMU_ADDRESS, 2000, i_IMU);
+
+/* FIXME - LSM6DSM i_IMU;
+IMU imu(IMU_ADDRESS, 2000, i_IMU); */
+
+Adafruit_BMP085 e_Barometer;
+Barometer barometer(e_Barometer);
 
 //* GLOBAL VARIABLES
 
 //* FUNCTIONS
 
 void setup() {
-  http.begin(HTTP_ADDRESS); //! Don't forget to change IP
-	http.addHeader("Content-Type", "application/json");
+	/* FIXME - http.begin(HTTP_ADDRESS); //! Don't forget to change IP
+	http.addHeader("Content-Type", "application/json"); */
 
 	Serial.begin(115200);
 	Serial.println("Serial started.");
 
-  // Initialize WiFi
-  Wifi::init(WIFI_SSID, WIFI_PASSWORD);
+	// Initialize WiFi
+	// FIXME - Wifi::init(WIFI_SSID, WIFI_PASSWORD);
+
+	// Initialize IMU
+	/* imu.startIMU(); */
+
+	// Initialize Barometer
+	barometer.startBarometer();
 }
 
 void loop() {
+	barometer.gatherData();
+	delay(50);
+	barometer.printData();
+	delay(50);
 }
